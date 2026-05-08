70760d077b82301fda97bb4a86de097ed6bdcd0c
LazyPanda07 <semengricenko@gmail.com>
2026-05-08 18:46:14 +0500

    Add baseline11

---
+++ frameworks/web-framework-cpp/web-framework-benchmark/server/server.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <import.hpp>
+
+int main(int argc, char** argv) try
+{
+	framework::utility::initializeWebFramework();
+
+	framework::utility::Config config("config.json");
+
+	framework::WebFramework server(config);
+
+	server.start(true, []() { std::cout << "Server is running..." << std::endl; });
+
+	return 0;
+}
+catch (const std::exception& e)
+{
+	std::cerr << e.what() << std::endl;
+
+	return 1;
+}
