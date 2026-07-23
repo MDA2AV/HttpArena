6d8071fd8e1eaecbf0bb78c29e5cd699d8372933
prothegee <prothegee@proton.me>
2026-07-23 07:12:45 +0700

    collapse handler signatures and sends

--- frameworks/zix/src/handlers/pipeline.zig
+++ frameworks/zix/src/handlers/pipeline.zig
@@ -12,19 +12,10 @@ const PIPELINE_RESP: []const u8 =
 
 // --------------------------------------------------------- //
 
-pub fn RESPONSE(
-    req: *zix.Http1.Request,
-    _: *zix.Http1.Response,
-    _: *zix.Http1.Context
-) !void {
+pub fn RESPONSE(req: *zix.Http1.Request, _: *zix.Http1.Response, _: *zix.Http1.Context) !void {
     const fd = req.fd;
 
     zix.Http1.writeAllFD(req.fd, PIPELINE_RESP) catch {
-        try zix.Http1.sendSimpleFD(
-            fd,
-            @intFromEnum(zix.Http1.Status.Code.INTERNAL_SERVER_ERROR),
-            zix.Http1.Content.Type.TEXT_PLAIN.asString(),
-            zix.Http1.Status.Code.INTERNAL_SERVER_ERROR.asString()
-        );
+        try zix.Http1.sendSimpleFD(fd, @intFromEnum(zix.Http1.Status.Code.INTERNAL_SERVER_ERROR), zix.Http1.Content.Type.TEXT_PLAIN.asString(), zix.Http1.Status.Code.INTERNAL_SERVER_ERROR.asString());
     };
 }
