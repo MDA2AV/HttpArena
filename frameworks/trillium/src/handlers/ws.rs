bfa66c353783f3edb2438ce78d8ff70dd446a98e
Jacob Rothstein <hi@jbr.me>
2026-08-21 13:56:11 -0700

    [trillium.rs] update dependencies and use websocket message coalescing

--- frameworks/trillium/src/handlers/ws.rs
+++ frameworks/trillium/src/handlers/ws.rs
@@ -1,15 +1,15 @@
+use futures_lite::StreamExt;
 use trillium_websockets::{Message, WebSocketConn};
 
 pub async fn ws_echo(mut conn: WebSocketConn) {
-    use futures_lite::StreamExt;
     while let Some(Ok(msg)) = conn.next().await {
         match &msg {
             Message::Ping(_) | Message::Pong(_) | Message::Frame(_) => continue,
             Message::Close(_) => break,
             Message::Text(_) | Message::Binary(_) => {}
         }
-        if let Err(e) = conn.send(msg).await {
-            log::debug!("ws send failed: {e}");
+        if let Err(e) = conn.feed(msg).await {
+            log::debug!("ws feed failed: {e}");
             break;
         }
     }
