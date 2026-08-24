978e577689df02da44c4a8a7330432f28f2af3dd
Petrik <petrik@deheus.net>
2026-08-24 21:16:35 +0200

    [sinatra] Simplify total calculation for baseline11
    This makes it consistent with other calls where we also sum the a and b
    params.

--- frameworks/sinatra/app.rb
+++ frameworks/sinatra/app.rb
@@ -72,10 +72,7 @@ class App < Sinatra::Base
   end
 
   get('/baseline11') do
-    total = 0
-    request.GET.each do |_k, v|
-      total += v.to_i
-    end
+    total = params['a'].to_i + params['b'].to_i
     render_plain total.to_s
   end
 
