ea3e92c41e6a0151be84aa8d39d66e1d1d9b821b
Petrik <petrik@deheus.net>
2026-08-27 19:18:55 +0200

    [hanami] Improve Hanami implementation
    Standardize with other Ruby frameworks
    Implement json-tls.
    Set logging level to error for production.
    Add Gemfile.lock to improve reproducibility.

--- frameworks/hanami/app/action.rb
+++ frameworks/hanami/app/action.rb
@@ -3,22 +3,5 @@ require "rack"
 
 module Arena
   class Action < Hanami::Action
-    private
-
-    # The benchmark posts bodies with no Content-Type, which Rack parses as form
-    # data, so the query is read from QUERY_STRING and the body from rack.input.
-    def query_sum(request)
-      ::Rack::Utils.parse_query(request.env["QUERY_STRING"]).values.sum do |value|
-        Array(value).first.to_i
-      end
-    end
-
-    def request_body(request)
-      input = request.env["rack.input"]
-      return "" unless input
-
-      input.rewind
-      input.read.to_s
-    end
   end
 end
